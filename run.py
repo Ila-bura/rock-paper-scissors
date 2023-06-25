4a7eefc7ecd51ed775ae1c8d3641f09620d11080
Ila-bura <>
2023-06-25 16:07:29 +0000

    Create variables for scores and possible game options

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import random
 
+"""
+Initial print statements to greet the user and introduce the game
+"""
+
 print("Welcome to the game of Rock Paper Scissors!")
 print("-------------------------------------------")
 print("A few fun facts about this game:")
@@ -8,3 +12,11 @@ print("Statistically people choose Scissors in the first round")
 print("and Rock in the second.")
 print("A robot developed in Japan wins with 100 per cent chance.")
 print("It analyzes movement of your hand muscles to predict your choice.")
+
+"""
+Create game variables
+"""
+userScore = 0
+computerScore = 0
+tiedScore = 0
+gameOptions = ["Rock", "Paper", "Scissors"]
