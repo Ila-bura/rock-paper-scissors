b50cb5716b3d9e567e6d5f4a716bfbd8d3096a83
Ila-bura <>
2023-06-25 16:00:47 +0000

    Add initial print statements to greet the user and present the game

---
+++ run.py
@@ -0,0 +1,10 @@
+import random
+
+print("Welcome to the game of Rock Paper Scissors!")
+print("-------------------------------------------")
+print("A few fun facts about this game:")
+print("It first appeared in China in the 17th century.")
+print("Statistically people choose Scissors in the first round")
+print("and Rock in the second.")
+print("A robot developed in Japan wins with 100 per cent chance.")
+print("It analyzes movement of your hand muscles to predict your choice.")
