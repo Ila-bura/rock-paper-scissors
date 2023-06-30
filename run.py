5422006ee950f73b6306f8c2a5b811db62e8124b
Ila-bura <>
2023-06-30 17:41:16 +0000

    Split code into functions following mentor's feedback

--- run.py
+++ run.py
@@ -27,40 +27,46 @@ gameOptions = ["rock", "paper", "scissors"]
 While loop to ask the user if they need a refresher of the rules of the game.
 Check for valid input and prompt the user to enter the two acceptable options.
 """
+# function to disply the rules
 
-while True:
+
+def display_rules():
     rulesRecap = input("Do you need a refresher of the rules? Y/N: ").lower()
     if rulesRecap == "y":
         print("- Rock smashes Scissors, Scissors cut Paper, Paper covers Rock")
         print("- Every turn you win, you score 1 point")
         print("- To win the game you need a total of 3 points")
-        break
+        return
     elif rulesRecap == "n":
         # code to continue without the recap
-        break
+        return
     else:
         print("Invalid input. Please enter Y or N.")
+
 
 """
 Function to kick off the game. Check if the option typed is valid.
 Show option to abandon the game.
 """
 
-while True:
+
+def get_user_selection():
     userTurn = input("\nPick Rock, Paper, Scissors or Q to quit: ").lower()
     if userTurn == "q":
-        break
+        return print("You chose", userTurn.capitalize() + ".")
     if userTurn not in gameOptions:
         print("Invalid input: you can only choose Rock, Paper or Scissors.")
-        print("Please, check your spelling and try again!")
-        continue
+        print("Please, check your spelling and try again!")      
 
+
+def get_bot_selection(userTurn):
     random_number = random.randint(0, 2)
     # index number: rock 0, paper 1, scissors 2
     computerTurn = gameOptions[random_number]
-    print("You chose", userTurn.capitalize() + ".")
     print("Computer chose", computerTurn.capitalize() + ".")
 
+
+def decide_winner(userTurn, computerTurn):
     if userTurn == "rock" and computerTurn == "scissors":
         print("Rock smashes scissors! You win!")
         userScore += 1
@@ -89,27 +95,45 @@ while True:
         print("It's a tie! Play again")
         tiedScore += 1
     # function to show final scores as soon as 3 points are awarded
+
+
+def show_scores(userScore, botScore, tiedScore):
     if userScore == 3 or botScore == 3:
         print("-------------------------------------------")
         print("Game over!")
-        if userScore > botScore:
-            print("You won the game!")
-            print("You scored: ", userScore, "Computer scored: ", botScore)
-            print("Ties: ", tiedScore)
-            print("-------------------------------------------")
-            print("Thanks for playing!")
-        else:
-            print("Computer won the game!")
-            print("You scored: ", userScore, "Computer scored: ", botScore)
-            print("Ties: ", tiedScore)
-            print("-------------------------------------------")
-            print("Losing sucks, but thanks for playing!")
-    # function to ask the user to play agin and if so, reset the scores
-        playAgain = input("Do you want to give it another go? (Y/N): ")
-        if playAgain.lower() != "n":
-            userScore = 0
-            botScore = 0
-            tiedScore = 0
-            userTurn
-        else:
-            break
+    if userScore > botScore:
+        print("You won the game!")
+        print("You scored: ", userScore, "Computer scored: ", botScore)
+        print("Ties: ", tiedScore)
+        print("-------------------------------------------")
+        print("Thanks for playing!")
+    else:
+        print("Computer won the game!")
+        print("You scored: ", userScore, "Computer scored: ", botScore)
+        print("Ties: ", tiedScore)
+        print("-------------------------------------------")
+        print("Losing sucks, but thanks for playing!")
+    # function to ask the user to play again and if so, reset the scores
+
+
+def playAgain():
+    answer = input("Do you want to give it another go? (Y/N): ")
+    if answer.lower() != "n":
+        userScore = 0
+        botScore = 0
+        tiedScore = 0
+        userTurn = True
+    else:
+        pass
+
+def main():
+    """
+    Call all program functions.
+    """
+    display_rules()
+    decide_winner(userTurn, computerTurn)
+    show_scores()
+    playAgain()
+
+
+main()
