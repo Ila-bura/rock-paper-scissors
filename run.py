3db74f5d3fb9ac3e79bcb2254201c39e6a93121b
Ila-bura <>
2023-07-02 16:23:17 +0000

    Add \n to print staments to improve readability

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import random
-# list of acceptable options for the game
+# constant with a list of acceptable options for the game
 gameOptions = ["rock", "paper", "scissors"]
 
 """
@@ -7,13 +7,13 @@ Initial print statements to greet the user,
 provide fun facts and introduce the game
 """
 
-print("Welcome to the game of Rock Paper Scissors!")
+print("\nWelcome to the game of Rock Paper Scissors!")
 print("-------------------------------------------")
-print("Two fun facts:")
-print("1. It first appeared in China in the 17th century.")
-print("2. Statistically, people tend to choose Scissors in the first round")
-print("   and Rock in the second.")
-print("Now, let's play!")
+print("\nTwo fun facts:")
+print("\n1. It first appeared in China in the 17th century.")
+print("\n2. Statistically, people tend to choose Scissors in the first round")
+print("\n   and Rock in the second.")
+print("\nNow, let's play!")
 print("-------------------------------------------")
 
 """
@@ -25,13 +25,14 @@ Check for valid input and prompt the user to enter the two acceptable options.
 def display_rules():
     rulesRecap = input("Do you need a refresher of the rules? Y/N:\n").lower()
     if rulesRecap == "y":
-        print("- Rock smashes Scissors, Scissors cut Paper, Paper covers Rock")
-        print("- Every turn you win, you score 1 point")
-        print("- To win the game you need a total of 3 points")
+        print("\n- Rock smashes Scissors, Scissors cut Paper")
+        print("\n- Paper covers Rock")
+        print("\n- Every turn you win, you score 1 point")
+        print("\n- To win the game you need a total of 3 points")
     elif rulesRecap == "n":
         pass
     else:
-        print("Invalid input. Please enter Y or N.")
+        print("\nInvalid input. Please enter Y or N.")
 
 
 """
@@ -54,13 +55,13 @@ Function to display the game results at every turn
 
 def show_results(userTurn, bot_choice):
     if userTurn == bot_choice:
-        print("It's a tie! Play again.")
+        print("\nIt's a tie! Play again.")
     elif (userTurn == "rock" and bot_choice == "scissors") or \
             (userTurn == "paper" and bot_choice == "rock") or \
             (userTurn == "scissors" and bot_choice == "paper"):
-        print("You win!")
+        print("\nYou win!")
     else:
-        print("You lose!")
+        print("\nYou lose!")
 
 
 """
@@ -79,34 +80,35 @@ def let_us_play():
         userTurn = input("\nPick Rock/Paper/Scissors or Q to quit:\n").lower()
 
         if userTurn == "q":
-            print("Game over! Thanks for playing!")
+            print("\nGame over! Thanks for playing!")
             break
 
         if userTurn not in gameOptions:
-            print("Invalid input: you can only choose Rock, Paper or Scissors")
-            print("Please, check your spelling and try again!")
+            print("\nInvalid input")
+            print("\nYou can only choose Rock, Paper or Scissors")
+            print("\nPlease, check your spelling and try again!")
             continue
 
         bot_choice = get_bot_selection()
-        print("You chose:", userTurn)
-        print("Bot chose:", bot_choice)
+        print("\nYou chose:", userTurn)
+        print("\nBot chose:", bot_choice)
 
         if userTurn == bot_choice:
             tie_score += 1
-            print("It's a tie! Play again!")
+            print("\nIt's a tie! Play again!")
         elif (userTurn == "rock" and bot_choice == "scissors") or \
                 (userTurn == "paper" and bot_choice == "rock") or \
                 (userTurn == "scissors" and bot_choice == "paper"):
             user_score += 1
-            print("You win!")
+            print("\nYou win!")
         else:
             bot_score += 1
-            print("You lose!")
+            print("\nYou lose!")
 
         if user_score == 3 or bot_score == 3:
-            print("Game over!")
-            print("You scored:", user_score)
-            print("Bot scored:", bot_score)
+            print("\nGame over!")
+            print("\nYou scored:", user_score)
+            print("\nBot scored:", bot_score)
             print("Ties:", tie_score)
             break
 
