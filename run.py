17160b3c54b8a36074a23b8bae43dc17cee1eb37
Ila-bura <>
2023-07-07 17:49:07 +0000

    Fix bug when user enters incorrect input more than once

--- run.py
+++ run.py
@@ -25,14 +25,17 @@ Check for valid input and prompt the user to enter the two acceptable options.
 def display_rules():
     rulesRecap = input("\n Do you need to refresh the rules? Y/N:\n").lower()
     if rulesRecap == "y":
-        print("\n - Rock smashes Scissors, Scissors cut Paper")
+        print("\n - Rock smashes Scissors")
+        print("\n - Scissors cut Paper")
         print("\n - Paper covers Rock")
         print("\n - Every turn you win, you score 1 point")
         print("\n - To win the game you need a total of 3 points")
+        print("-------------------------------------------")
     elif rulesRecap == "n":
         pass
     else:
         print("\n Invalid input. Please enter Y or N.")
+        display_rules()
 
 
 """
@@ -113,15 +116,21 @@ def let_us_play():
                 print("\n You scored:", user_score)
                 print("\n Bot scored:", bot_score)
                 print("\n Ties:", tie_score)
-                break
             else:
                 print("\n Bot won the game!")
                 print("\n You scored:", user_score)
                 print("\n Bot scored:", bot_score)
                 print("\n Ties:", tie_score)
-                print("-------------------------------------------")
                 print("\n Losing sucks, but thanks for playing!")
-                break
+            print("-------------------------------------------")
+            play_again()
+
+
+"""
+Function to ask the user if they want to play another round
+at the end of the first game.
+
+"""
 
 
 def play_again():
@@ -129,6 +138,12 @@ def play_again():
     if want_more == "y":
         print("\n Here you go, best of luck!")
         let_us_play()
+    elif want_more == "n":
+        print("\n Thanks for playing!")
+        break
+    else:
+        print("\n Invalid input. Please enter Y or N.")
+        play_again()
 
 
 def main():
@@ -137,7 +152,6 @@ def main():
     """
     display_rules()
     let_us_play()
-    play_again()
 
 
 main()
