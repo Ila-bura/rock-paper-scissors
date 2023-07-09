25dc1b6ef5c1219a47f63a351254907ac60aa04c
Ila-bura <>
2023-07-09 15:23:49 +0000

    Refactor code to create a more organized structure

--- run.py
+++ run.py
@@ -82,6 +82,49 @@ def display_game_result():
         print("Sorry, losing sucks")
 
 
+# function to ask the user if they want to restart or quit the game
+def ask_restart_quit():
+    while True:
+        choice = input("\nDo you want to play again? (Y)es/(Q)uit: ").lower()
+        if choice == "y":
+            reset_scores()
+            print("\nHere you go, best of luck!")
+            let_us_play()
+            break
+        elif choice == "q":
+            print("\nTodaloo & thanks for playing!")
+            sys.exit()
+        else:
+            print("Invalid input. Please enter Y or Q.")
+
+
+# function to kick off the game.
+def let_us_play():
+    while True:
+        user_turn = get_user_selection()
+        bot_choice = get_bot_selection()
+        print("\n You chose:", user_turn)
+        print("\n Bot chose:", bot_choice)
+
+        result = decide_winner(user_turn, bot_choice)
+        update_scores(result)
+
+        display_result(result)
+
+        if round_over():
+            display_game_result()
+            ask_restart_quit()
+            break
+
+
+# function to reset the scores
+def reset_scores():
+    global user_score, bot_score, tie_score
+    user_score = 0
+    bot_score = 0
+    tie_score = 0
+
+
 # function to ask if the user needs a refresher of the rules
 def ask_rules_refresh():
     rules_recap = input("\n Do you need to refresh the rules? Y/N:\n").lower()
@@ -105,63 +148,7 @@ def display_rules():
     return None
 
 
-# function to reset the scores
-def reset_scores():
-    global user_score, bot_score, tie_score
-    user_score = 0
-    bot_score = 0
-    tie_score = 0
-
-
-# function to ask the user if they want to restart or quit the game
-def ask_restart_quit():
-    while True:
-        choice = input("\nDo you want to play again? (Y)es/(Q)uit: ").lower()
-        if choice == "y":
-            reset_scores()
-            print("\nHere you go, best of luck!")
-            let_us_play()
-            break
-        elif choice == "q":
-            print("\nTodaloo & thanks for playing!")
-            sys.exit()
-        else:
-            print("Invalid input. Please enter Y or Q.")
-
-
-# function to kick off the game. Prompt the user to choose an option.
-def let_us_play():
-    while True:
-        user_turn = get_user_selection()
-        bot_choice = get_bot_selection()
-        print("\n You chose:", user_turn)
-        print("\n Bot chose:", bot_choice)
-
-        result = decide_winner(user_turn, bot_choice)
-        update_scores(result)
-
-        if result == "tie":
-            print("\n It's a tie, play again!")
-        elif result == "user":
-            print("\n You win!")
-        else:
-            print("\n You lose!")
-
-        if user_score == 3 or bot_score == 3:
-            print("-------------------------------------------")
-            print("\n Game over!")
-            print("\n You scored:", user_score)
-            print("\n Bot scored:", bot_score)
-            print("\n Ties:", tie_score)
-            if user_score > bot_score:
-                print("\n You won the game!")
-            else:
-                print("\n Bot won the game!")
-                print("\n Sorry, losing sucks")
-            ask_restart_quit()
-            break
-
-
+# main function to call all game functions
 def main():
     print("\n Welcome to the game of Rock Paper Scissors!")
     print("-------------------------------------------")
